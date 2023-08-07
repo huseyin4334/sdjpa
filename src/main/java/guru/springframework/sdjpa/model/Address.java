c5b23359f133e354f7daa6d44fcc2212e899465a
26075232 <huseyincan_kalcik@arcelik.com>
2023-08-07 12:01:54 +0300

    hibernate validation in code level

--- src/main/java/guru/springframework/sdjpa/model/Address.java
+++ src/main/java/guru/springframework/sdjpa/model/Address.java
@@ -2,11 +2,14 @@ package guru.springframework.sdjpa.model;
 
 import jakarta.persistence.Embeddable;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Embeddable
 @Data
 public class Address {
     private String address;
+
+    @Length(max = 20)
     private String city;
     private String state;
     private String zipCode;
--- src/main/java/guru/springframework/sdjpa/model/Customer.java
+++ src/main/java/guru/springframework/sdjpa/model/Customer.java
@@ -1,10 +1,14 @@
 package guru.springframework.sdjpa.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.Set;
 
@@ -15,13 +19,18 @@ import java.util.Set;
 @AllArgsConstructor
 public class Customer extends BaseEntity {
 
+    @Length(max = 50, message = "CustomerName can not be taller than 50 character")
     private String customerName;
 
+    @Valid // control class validations
     @Embedded
     private Address address;
 
+    @Length(max = 20)
     private String phone;
 
+    @Size(max = 255)
+    @Email
     private String email;
 
     @Embedded
